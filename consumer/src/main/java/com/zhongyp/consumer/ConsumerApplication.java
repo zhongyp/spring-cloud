f7b1c6bff673a7115f072af10fe40a87bcc2f0b8
zhongypb <zhongypb@yonyou.com>
2019-12-14 20:54:04 +0800

    重新设计项目结构

--- consumer/src/main/java/com/zhongyp/consumer/ConsumerApplication.java
+++ consumer/src/main/java/com/zhongyp/consumer/ConsumerApplication.java
@@ -2,12 +2,21 @@ package com.zhongyp.consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableDistributedTransaction
 public class ConsumerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ConsumerApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 
 }
